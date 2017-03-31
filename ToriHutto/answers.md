5a44fa4ac53cbbfd04c913bd48d2487114a905e0
torservo82 <toritimberwolf@gmail.com>
2017-03-31 09:15:33 -0400

    added JS4 hw

---
+++ ToriHutto/answers.md
@@ -0,0 +1,56 @@
+JS4 ANSWERS
+1.You need to create a report that shows the total sales figures for each of the sales people in your company. What construct must you use to accomplish this?
+
+a. conditional
+b. loop
+c. exception
+d. function
+
+B. loop
+
+
+2.What type of statement would you use if you wanted to execute a single block of code under a specific circumstance?
+
+a. if
+b. if/else
+c. if/else if/else
+d. all of the above
+
+A. if
+
+
+3. While loops execute until:
+
+a. The conditional expression evaluates to true.
+b. The conditional expression evaluates to false.
+
+B.
+
+
+4. What keyword is used to immediately stop execution of a loop?
+
+a. goto
+b. exit
+c. break 
+d. continue
+
+C. Break
+
+5. Which of the following are true about functions in JavaScript?
+
+a. Functions facilitate code reuse
+b. Functions can receive data via arguments when executed
+c. Functions can evaluate to a value
+d. All of the above
+
+D. All of the above
+
+
+What is the appropriate syntax for invoking a function named sayHello that accepts a single argument, "Fred"?
+
+a. sayHello
+b. sayHello["Fred"]
+c."Fred".sayHello()
+d. sayHell0("Fred") 
+
+D. sayHell0("Fred")
---
+++ ToriHutto/exercises.js
@@ -0,0 +1,94 @@
+EXERCISES JS4
+
+
+function printStanding(GPA) {
+	var result;
+  if(GPA >= 2.0){
+  	result= "Student is in good standing";
+  } else {
+  	result= "Student is on probation";
+  }
+  if(GPA >= 3.40 && GPA <=3.59){
+  	result= result + " with cum laude";
+  } else if (GPA >= 3.60 && GPA <=3.79){
+  	result= result + " with magna cum laude";
+  } else if (GPA >= 3.80 && GPA <= 4.00){
+  	result= result + " with summa cum laude";
+  }
+  
+		console.log(result);
+}
+printStanding(3.5);
+printStanding(2.0);
+printStanding(4.0);
+printStanding(1.3);
+
+
+2)
+var i;
+for(i=0; i <= 99; i = i + 3){
+	console.log(i);
+}
+
+
+
+3) 
+
+var currentTime;
+while( currentTime= new Date().valueOf()){
+	var remainder= currentTime % 100;
+  if (remainder == 0 ){
+  	break;
+  }
+  console.log(currentTime);
+}
+
+4.) Leap years are defined as being a year that is divisible by 4, except for years which are divisible by 100 and not divisible 400.
+
+For example:
+
+* 2004 is divisible by 4, and is not divisible by 100, so it's a leap year.
+* 2000 is divisible by 4, 100 and 400, so it's a leap year.
+* 1700 is divisible by 4 and 100, but not 400, so it's not a leap year.
+
+You will output all of the leap years from the year 0 to 2020 by doing the following steps:
+
+1. Write out pseudocode that describes the process you would need to go through to complete this requirement.
+2. Translate the pseudocode into real JavaScript that accomplishes the requirement.
+
+
+
+5.) A result variable has been declared for you, as well as a function named half. Call the half function with the number 100 and store the result in the result variable.
+
+
+var result;
+ 
+ function half(number){
+ 	 return number / 2;
+ }
+ 
+ result = half(100);
+ 
+ console.log(result);
+
+
+
+
+
+6.) Define a function called difference, with two parameters, that returns the difference between two values.
+
+function difference ()
+
+var result = difference(385, 142);
+
+function difference(num1, num2){
+	return num1 - num2;
+ };
+ 
+ var result = difference(385, 142);
+ 
+ console.log(result);
+
+
+
+
